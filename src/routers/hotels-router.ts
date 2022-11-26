d9c574bca289826b739161e960ebe7c6ef05b40c
Sergio <sergiomendes23@gmail.com>
2022-11-26 02:04:53 -0300

    hotels service testing

--- src/routers/hotels-router.ts
+++ src/routers/hotels-router.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { authenticateToken } from "@/middlewares";
-import { getHotels } from "@/controllers/hotels-controller";
+import { getHotels, getRoomsHotels } from "@/controllers/hotels-controller";
 
 const hotelsRouter = Router();
 
 hotelsRouter
   .all("/*", authenticateToken)
-  .get("/", getHotels);
+  .get("/", getHotels)
+  .get("/:hotelId", getRoomsHotels);
 
 export { hotelsRouter };
